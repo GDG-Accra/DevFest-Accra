fe54006c4a6b95c49a706ee4c3e752f9ad61517e
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2025-05-03 23:45:04 +0000

    about section text sizes revised

--- src/Components/AboutSection/AboutSection.tsx
+++ src/Components/AboutSection/AboutSection.tsx
@@ -4,9 +4,23 @@ import { Heart } from "../../assets/elements/heart";
 
 export const AboutSection = () => {
   return (
-    <section className="w-full relative flex flex-col gap-4 px-4 lg:px-[60.5px] md:gap-5 lg:gap-6 2xl:w-[1391px] 2xl:gap-[38px] my-52">
-      <div className="flex gap-3 md:gap-4 2xl:w-[485.91px] 2xl:gap-[20px] justify-center items-center xl:justify-start">
-        <h1 className="text-3xl md:text-[40px] lg:text-[50px] leading-tight md:leading-none font-inter font-bold text-[#1E1E1E] 2xl:text-[50px] text-center xl:text-left">
+    <section className="w-full relative flex flex-col gap-4 px-4 mx-auto
+      md:gap-5 
+      lg:px-[60.5px] lg:gap-6 
+      xl:items-center
+      2xl:w-[1391px] 2xl:gap-[38px] 
+      3xl:w-[1512px]
+      my-52">
+      <div className="flex items-center w-full gap-3
+        md:gap-4 
+        lg:justify-start
+        xl:justify-start
+        2xl:w-[485.91px] 2xl:gap-[20px]">
+        <h1 className="text-3xl font-inter font-bold text-[#1E1E1E] leading-tight
+          md:text-[40px] md:leading-none
+          lg:text-[50px]
+          2xl:text-[50px]
+          lg:text-left">
           About&nbsp;
           <br className="md:mb-2 lg:mb-3" />
           DevFest Accra
@@ -16,7 +30,11 @@ export const AboutSection = () => {
       </div>
 
       <div className="flex flex-col xl:flex-row items-center justify-between w-full gap-6 md:gap-8 2xl:w-[1391px] 2xl:h-[538.5px]">
-        <p className="text-lg md:text-[23px] lg:text-[30px] leading-[130%] xl:leading-[110%] font-normal text-[#444444] lg:max-w-[50%]">
+        <p className="text-lg leading-normal font-normal text-[#444444] lg:max-w-[50%]
+          md:text-[20px]
+          lg:text-[24px]
+          2xl:text-[30px]
+          3xl:text-[36px]">
           DevFest Accra brings this global experience to Ghana, uniting local
           developers, designers, and industry experts in an environment that
           fosters learning and innovation. It serves as a platform for
