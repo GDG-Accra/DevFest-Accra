d52bb91d90f7bceb49fc687fd971a98e833ea7a7
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2025-07-19 07:35:24 +0000

    Update Homepage.tsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import SpeakersSection from "../Components/SpeakersSection/SpeakersSection";
 import { AboutSection } from "../Components/AboutSection/AboutSection";
 import ThrowbackSection from "../Components/ThrowbackSection/ThrowbackSection";
