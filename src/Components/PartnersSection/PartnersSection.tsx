598aaeb51fa6370b4c348e6594529f22934c4271
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2025-07-23 23:36:15 +0000

    Adding more Sponsors and Partners (#161)

--- src/Components/PartnersSection/PartnersSection.tsx
+++ src/Components/PartnersSection/PartnersSection.tsx
@@ -4,16 +4,17 @@ import GDGLogo from "../../assets/images/Partners/logos/gdg.svg";
 import ATULibraryLogo from "../../assets/images/Partners/logos/atu-library.svg";
 import GamersHiveLogo from "../../assets/images/Partners/logos/gamers-hive.svg";
 import InfobipLogo from "../../assets/images/Partners/logos/infobib.svg";
+import MestLogo from "../../assets/images/Partners/logos/mest.svg";
 import HashLogo from "../../assets/images/Partners/logos/hash.svg";
 import SlashLogo from "../../assets/images/Partners/logos/slash.svg";
+import KraadoLogo from "../../assets/images/Partners/logos/kraado.svg";
 
 const PartnersSection: React.FC = () => {
   return (
     <section className='text-center py-16 px-4 bg-white'>
-      
-      <h2 className="max-w-xl mx-auto text-gray-700 mb-2 md:mb-4 uppercase">
-      Sponsors | Partners
-				</h2>
+      <h2 className='max-w-xl mx-auto text-gray-700 mb-2 md:mb-4 uppercase'>
+        Sponsors | Partners
+      </h2>
 
       <div className='relative max-w-5xl mx-auto mb-4'>
         <img
@@ -25,7 +26,8 @@ const PartnersSection: React.FC = () => {
 
         <h1 className='text-4xl lg:text-5xl font-bold  text-center'>
           Powered by the Ones Who Believe
-          <br/>Our Sponsors and Partners.
+          <br />
+          Our Sponsors and Partners.
         </h1>
       </div>
 
@@ -36,18 +38,17 @@ const PartnersSection: React.FC = () => {
 
       {/* Logos section with responsive grid */}
       <section className='max-w-5xl mx-auto'>
-        <div className='grid grid-cols-2 md:grid-cols-4 gap-8 place-items-center'>
+        <div className='grid md:grid-cols-3 grid-cols-2 gap-6'>
           {/* 1 */}
-          <div className='flex items-center justify-center p-4 h-32 md:h-40 lg:h-48 w-full max-w-xs'>
+          <div className='flex items-center justify-center'>
             <img
               src={GDGLogo}
               alt='GDG'
               className='max-h-full w-full object-contain'
             />
           </div>
-          
           {/* 2 */}
-          <div className='flex items-center justify-center p-4 h-32 md:h-40 lg:h-48 w-full max-w-xs'>
+          <div className='flex items-center justify-center'>
             <img
               src={ATULibraryLogo}
               alt='ATU Library'
@@ -55,7 +56,7 @@ const PartnersSection: React.FC = () => {
             />
           </div>
           {/* 3 */}
-          <div className='flex items-center justify-center p-4 h-32 md:h-40 lg:h-48 w-full max-w-xs'>
+          <div className='flex items-center justify-center'>
             <img
               src={GamersHiveLogo}
               alt='Gamers Hive'
@@ -63,27 +64,40 @@ const PartnersSection: React.FC = () => {
             />
           </div>
           {/* 4 */}
-          <div className='flex items-center justify-center p-4 h-32 md:h-40 lg:h-48 w-full max-w-xs'>
+          <div className='flex items-center justify-center'>
             <img
               src={InfobipLogo}
               alt='Infobip'
               className='max-h-full w-full object-contain'
             />
           </div>
-          
-           <div className='flex items-center justify-center p-4 h-32 md:h-40 lg:h-48 w-full max-w-xs'>
+          {/* 5 */}
+          <div className='flex items-center justify-center'>
             <img
-              src={InfobipLogo}
-              alt='Infobip'
+              src={MestLogo}
+              alt='Mest'
               className='max-h-full w-full object-contain'
             />
           </div>
-        
+
+          {/* 6 */}
+          <div className='flex items-center justify-center'>
+            <img
+              src={KraadoLogo}
+              alt='Kraado'
+              className='max-h-full w-full object-contain'
+            />
+          </div>
         </div>
       </section>
 
-      <div className='lg:w-full  flex justify-end mt-6 md:mt-[-110px]'>
-        <img src={SlashLogo} alt='Slash Logo' className='h-10 md:h-12 lg:h-20 m-0 lg:mr-20' />
+      <div className='lg:w-full  flex justify-end mt-6 '>
+        {/* md:mt-[-110px] */}
+        <img
+          src={SlashLogo}
+          alt='Slash Logo'
+          className='h-10 md:h-12 lg:h-20 m-0 lg:mr-20'
+        />
       </div>
     </section>
   );
