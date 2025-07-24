c66f3f92ef25eea87bcdb4cdbc4cff76c49fd12f
Armarfio <85256457+Armarfio@users.noreply.github.com>
2025-07-24 17:19:15 +0000

    updated the page with the fix for footer showing (#165)
    the speakers page was showing the buttom or footer for the page.

--- src/pages/SpeakersPage/SpeakerPage.tsx
+++ src/pages/SpeakersPage/SpeakerPage.tsx
@@ -8,6 +8,11 @@ import codeRight from "../../assets/images/TeamImages/codeRight.svg";
 import Speakers from "./Speakers";
 
 const SpeakerPage = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scrolls to the top of the page
+  }, []);
+  
   return (
     <div>
       <Speakers />
