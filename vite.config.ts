2e9ac589fc964c17c87194e5cffa647a762e2bb1
Sosu Alfred <sosualfred@gmail.com>
2025-07-25 05:22:11 +0000

    Refactor Vite configuration and asset imports to use relative paths
    - Removed root and alias settings from vite.config.ts for a cleaner configuration.
    - Updated asset imports in assets.ts to use relative paths, enhancing maintainability and consistency across the project.

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: ".",
-  resolve: {
-    alias: {
-      "/src": "./src",
-    },
-  },
   server: {
     allowedHosts: [".gdgaccra.org", ".traefik.me"],
   },
