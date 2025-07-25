abc32ffa867d309083aa3f10f287a74cb930286c
Sosu Alfred <sosualfred@gmail.com>
2025-07-25 05:16:44 +0000

    Update Vite configuration and asset imports
    - Set the project root in vite.config.ts and added an alias for absolute path imports.
    - Refactored asset imports in assets.ts to use absolute paths, improving clarity and maintainability across the project.

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,57 +1,57 @@
-// Centralized asset imports
-import AboutImage from "./images/About/AboutImage.svg";
-import BefirstImage from "./images/BeFirst/BeImage.svg";
-import DPImage from "./images/DP/DPImage.png";
-import Location from "./images/EventDetails/Location.png";
-import Calendar from "./images/EventDetails/Calendar.png";
-import faqLinkGif from "./images/FAQs/link.gif";
-import bgImage from "./images/LandingPageImage/bgImage.svg";
-import shape from "./images/LandingPageImage/shape.svg";
+// Centralized asset imports - using absolute paths from project root
+import AboutImage from "/src/assets/images/About/AboutImage.svg";
+import BefirstImage from "/src/assets/images/BeFirst/BeImage.svg";
+import DPImage from "/src/assets/images/DP/DPImage.png";
+import Location from "/src/assets/images/EventDetails/Location.png";
+import Calendar from "/src/assets/images/EventDetails/Calendar.png";
+import faqLinkGif from "/src/assets/images/FAQs/link.gif";
+import bgImage from "/src/assets/images/LandingPageImage/bgImage.svg";
+import shape from "/src/assets/images/LandingPageImage/shape.svg";
 
 // Logos
-import DF25AccraPNG from "./images/Logos/_DF25-Accra.png";
-import DF25AccraJPG from "./images/Logos/_DF25-Accra.jpg";
-import DF25LogoLockupYear from "./images/Logos/DF25-Logo-Lockup-year.png";
-import DF25LogoLockup from "./images/Logos/DF25-Logo-Lockup.png";
-import DFLogoAccra from "./images/Logos/DFLogo-Accra.svg";
-import DFShapeLogo from "./images/Logos/DFShapeLogo.svg";
-import GDGLogo from "./images/Logos/GDGLogo.png";
+import DF25AccraPNG from "/src/assets/images/Logos/_DF25-Accra.png";
+import DF25AccraJPG from "/src/assets/images/Logos/_DF25-Accra.jpg";
+import DF25LogoLockupYear from "/src/assets/images/Logos/DF25-Logo-Lockup-year.png";
+import DF25LogoLockup from "/src/assets/images/Logos/DF25-Logo-Lockup.png";
+import DFLogoAccra from "/src/assets/images/Logos/DFLogo-Accra.svg";
+import DFShapeLogo from "/src/assets/images/Logos/DFShapeLogo.svg";
+import GDGLogo from "/src/assets/images/Logos/GDGLogo.png";
 
 // Partners
-import gdgLogo from "./images/Partners/logos/gdg.svg";
-import atuLibraryLogo from "./images/Partners/logos/atu-library.svg";
-import gamersHiveLogo from "./images/Partners/logos/gamers-hive.svg";
-import infobibLogo from "./images/Partners/logos/infobib.svg";
-import hashLogo from "./images/Partners/logos/hash.svg";
-import slashLogo from "./images/Partners/logos/slash.svg";
-import kraadoLogo from "./images/Partners/logos/kraado.svg";
-import mestLogo from "./images/Partners/logos/mest.svg";
+import gdgLogo from "/src/assets/images/Partners/logos/gdg.svg";
+import atuLibraryLogo from "/src/assets/images/Partners/logos/atu-library.svg";
+import gamersHiveLogo from "/src/assets/images/Partners/logos/gamers-hive.svg";
+import infobibLogo from "/src/assets/images/Partners/logos/infobib.svg";
+import hashLogo from "/src/assets/images/Partners/logos/hash.svg";
+import slashLogo from "/src/assets/images/Partners/logos/slash.svg";
+import kraadoLogo from "/src/assets/images/Partners/logos/kraado.svg";
+import mestLogo from "/src/assets/images/Partners/logos/mest.svg";
 
 // Speakers
-import ShadrackInusahImage from "./images/Speakers/ShadrackInusah.svg";
-import OkaiAbenaImage from "./images/Speakers/OkaiAbena.svg";
-import SosuAlfredImage from "./images/Speakers/SosuAlfred.svg";
-import ShadrackOdameteyImage from "./images/Speakers/ShadrackOdametey.svg";
-import speakersShape from "./images/Speakers/shape.svg";
+import ShadrackInusahImage from "/src/assets/images/Speakers/ShadrackInusah.svg";
+import OkaiAbenaImage from "/src/assets/images/Speakers/OkaiAbena.svg";
+import SosuAlfredImage from "/src/assets/images/Speakers/SosuAlfred.svg";
+import ShadrackOdameteyImage from "/src/assets/images/Speakers/ShadrackOdametey.svg";
+import speakersShape from "/src/assets/images/Speakers/shape.svg";
 
 // Team Images
-import codeLeft from "./images/TeamImages/codeLeft.svg";
-import codeRight from "./images/TeamImages/codeRight.svg";
-import TeamBG from "./images/TeamImages/TeamBG.png";
-import TeamBGWebp from "./images/TeamImages/TeamBG.webp";
+import codeLeft from "/src/assets/images/TeamImages/codeLeft.svg";
+import codeRight from "/src/assets/images/TeamImages/codeRight.svg";
+import TeamBG from "/src/assets/images/TeamImages/TeamBG.png";
+import TeamBGWebp from "/src/assets/images/TeamImages/TeamBG.webp";
 
 // Throwback
-import throwbackImage from "./images/Throwback/throwbackImage.svg";
-import throwbackImageOne from "./images/Throwback/throwbackImageone.jpg";
-import throwbackImageTwo from "./images/Throwback/throwbackImagetwo.jpg";
-import throwbackImageThree from "./images/Throwback/throwbackImagethree.jpg";
-import throwbackImageFour from "./images/Throwback/throwbackImagefour.jpg";
-import throwbackImageFive from "./images/Throwback/throwbackImagefive.png";
+import throwbackImage from "/src/assets/images/Throwback/throwbackImage.svg";
+import throwbackImageOne from "/src/assets/images/Throwback/throwbackImageone.jpg";
+import throwbackImageTwo from "/src/assets/images/Throwback/throwbackImagetwo.jpg";
+import throwbackImageThree from "/src/assets/images/Throwback/throwbackImagethree.jpg";
+import throwbackImageFour from "/src/assets/images/Throwback/throwbackImagefour.jpg";
+import throwbackImageFive from "/src/assets/images/Throwback/throwbackImagefive.png";
 
 // DP Template
-import DPTemplateShape from "./DPTemplate/shape.svg";
-import DPTemplateSlash from "./DPTemplate/slash.svg";
-import DPTemplateLogo from "./DPTemplate/DFShapeLogo.svg";
+import DPTemplateShape from "/src/assets/DPTemplate/shape.svg";
+import DPTemplateSlash from "/src/assets/DPTemplate/slash.svg";
+import DPTemplateLogo from "/src/assets/DPTemplate/DFShapeLogo.svg";
 
 export const assets = {
   about: {
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: ".",
+  resolve: {
+    alias: {
+      "/src": "./src",
+    },
+  },
   server: {
     allowedHosts: [".gdgaccra.org", ".traefik.me"],
   },
