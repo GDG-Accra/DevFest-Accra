9963a32f7d59354115bd7dc7124b65a975afc414
Sosu Alfred <sosualfred@gmail.com>
2025-07-28 23:17:58 +0000

    use absolute paths

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,16 +1,16 @@
-import DFShapeLogo from "./DPTemplate/DFShapeLogo.svg";
-import shape from "./DPTemplate/shape.svg";
-import gdg from "./images/Partners/logos/gdg.svg";
-import atuLibrary from "./images/Partners/logos/atu-library.svg";
-import gamersHive from "./images/Partners/logos/gamers-hive.svg";
-import infobip from "./images/Partners/logos/infobib.svg";
-import hash from "./images/Partners/logos/hash.svg";
-import slash from "./DPTemplate/slash.svg";
-import mest from "./images/Partners/logos/mest.svg";
-import kraado from "./images/Partners/logos/kraado.svg";
-import faqLinkGif from "./images/FAQs/link.gif";
-import dpImage from "./images/DP/DPImage.png";
-import dpImage2 from "./images/DP/DPImage2.png";
+import DFShapeLogo from "/src/assets/DPTemplate/DFShapeLogo.svg";
+import shape from "/src/assets/DPTemplate/shape.svg";
+import gdg from "/src/assets/images/Partners/logos/gdg.svg";
+import atuLibrary from "/src/assets/images/Partners/logos/atu-library.svg";
+import gamersHive from "/src/assets/images/Partners/logos/gamers-hive.svg";
+import infobip from "/src/assets/images/Partners/logos/infobib.svg";
+import hash from "/src/assets/images/Partners/logos/hash.svg";
+import slash from "/src/assets/DPTemplate/slash.svg";
+import mest from "/src/assets/images/Partners/logos/mest.svg";
+import kraado from "/src/assets/images/Partners/logos/kraado.svg";
+import faqLinkGif from "/src/assets/images/FAQs/link.gif";
+import dpImage from "/src/assets/images/DP/DPImage.png";
+import dpImage2 from "/src/assets/images/DP/DPImage2.png";
 
 export const assets = {
   logos: {
