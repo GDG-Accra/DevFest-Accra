68f8579999f346fb86ab23a6a108759e08efcd46
Sosu Alfred <sosualfred@gmail.com>
2025-07-28 22:55:55 +0000

    Refactor PartnersSection to utilize centralized asset paths for all logos

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -2,8 +2,14 @@ export const assets = {
   logos: {
     dfShapeLogo: "/src/assets/DPTemplate/DFShapeLogo.svg",
     shape: "/src/assets/DPTemplate/shape.svg",
+    gdg: "/src/assets/images/Partners/logos/gdg.svg",
+    atuLibrary: "/src/assets/images/Partners/logos/atu-library.svg",
+    gamersHive: "/src/assets/images/Partners/logos/gamers-hive.svg",
+    infobip: "/src/assets/images/Partners/logos/infobib.svg",
+    hash: "src/assets/images/Partners/logos/hash.svg",
     slash: "/src/assets/DPTemplate/slash.svg",
-    kraado: "/src/assets/images/Partners/logos/kraado.svg"
+    mest: "/src/assets/images/Partners/logos/mest.svg",
+    kraado: "/src/assets/images/Partners/logos/kraado.svg",
   },
   gifs: {
     faqLinkGif: "/src/assets/images/FAQs/link.gif",
@@ -12,7 +18,4 @@ export const assets = {
     dpImage: "/src/assets/images/DP/DPImage.png",
     dpImage2: "/src/assets/images/DP/DPImage2.png",
   },
-  //  mestlogo: {
-  //   mestLinkGif: "src/assets/images/partners/logos/mest.svg",
-  // }
 };
