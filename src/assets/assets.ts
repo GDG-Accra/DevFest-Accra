cb229c01133ee0d71707a1d3c958f5a9cc6335d6
Sosu Alfred <sosualfred@gmail.com>
2025-07-28 23:24:01 +0000

    Refactor asset paths in assets.ts to use direct string references for improved clarity

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,19 +1,14 @@
-import DFShapeLogo from "/src/assets/DPTemplate/DFShapeLogo.svg";
-import shape from "/src/assets/DPTemplate/shape.svg";
-import faqLinkGif from "/src/assets/images/FAQs/link.gif";
-import dpImage from "/src/assets/images/DP/DPImage.png";
-import dpImage2 from "/src/assets/images/DP/DPImage2.png";
-
 export const assets = {
   logos: {
-    dfShapeLogo: DFShapeLogo,
-    shape: shape,
+    dfShapeLogo: "/src/assets/DPTemplate/DFShapeLogo.svg",
+    shape: "/src/assets/DPTemplate/shape.svg",
+    slash: "/src/assets/DPTemplate/slash.svg",
   },
   gifs: {
-    faqLinkGif: faqLinkGif,
+    faqLinkGif: "/src/assets/images/FAQs/link.gif",
   },
   images: {
-    dpImage: dpImage,
-    dpImage2: dpImage2,
+    dpImage: "/src/assets/images/DP/DPImage.png",
+    dpImage2: "/src/assets/images/DP/DPImage2.png",
   },
 };
